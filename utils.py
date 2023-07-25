2274bfadae5fd39c409eb44732d110c838f049f2
Prerana <prerana@hellosivi.com>
2023-07-25 07:07:49 +0530

    data augmentation

---
+++ utils.py
@@ -0,0 +1,39 @@
+from torchvision import datasets
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+means = [0.4914, 0.4822, 0.4465]
+stds = [0.2470, 0.2435, 0.2616]
+
+train_transforms = A.Compose(
+    [
+        A.Normalize(mean=means, std=stds, always_apply=True),
+        A.PadIfNeeded(min_height=4, min_width=4, always_apply=True),
+        A.RandomCrop(height=32, width=32, always_apply=True),
+        A.HorizontalFlip(),
+        A.CoarseDropout(max_holes=1, max_height=8, max_width=8, min_holes=1, min_height=8, min_width=8, fill_value=means),
+        ToTensorV2(),
+    ]
+)
+
+test_transforms = A.Compose(
+    [
+        A.Normalize(mean=means, std=stds, always_apply=True),
+        ToTensorV2(),
+    ]
+)
+
+class CIFAR10WithAlbumentations(datasets.CIFAR10):
+    def __init__(self, root="~/data/cifar10", train=True, download=True, transform=None):
+        super().__init__(root=root, train=train, download=download, transform=transform)
+        
+    def __getitem__(self, idx):
+        image, label = self.data[idx], self.targets[idx]
+
+        if self.transform is not None:
+            transformed = self.transform(image=image)
+            image = transformed["image"]
+            
+        return image, label
+    
