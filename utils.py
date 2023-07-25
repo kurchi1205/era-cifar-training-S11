92c1aad9b4f2b8f3715efadeb48c02fd0bb87e92
Prerana <prerana@hellosivi.com>
2023-07-25 07:08:29 +0530

    flip removed

--- utils.py
+++ utils.py
@@ -11,7 +11,6 @@ train_transforms = A.Compose(
         A.Normalize(mean=means, std=stds, always_apply=True),
         A.PadIfNeeded(min_height=4, min_width=4, always_apply=True),
         A.RandomCrop(height=32, width=32, always_apply=True),
-        A.HorizontalFlip(),
         A.CoarseDropout(max_holes=1, max_height=8, max_width=8, min_holes=1, min_height=8, min_width=8, fill_value=means),
         ToTensorV2(),
     ]
