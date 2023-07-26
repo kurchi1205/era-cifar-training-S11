2db40aa9b49bb41f65a9d8ef06eaff2b7c13910f
Prerana <prerana@hellosivi.com>
2023-07-26 21:22:13 +0530

    model summary

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from utils import CIFAR10WithAlbumentations, train_transforms, test_transforms
+from models.resnet18 import Resnet18
 import torch
+from torchsummary import summary
+
 cuda = torch.cuda.is_available()
+device = torch.device("cuda" if cuda else "cpu")
 
 def get_data():
     train_ds = CIFAR10WithAlbumentations('./data', train=True, download=True, transform=train_transforms)
@@ -12,3 +16,7 @@ def get_dataloader(data, shuffle=True, num_workers=4):
     dataloader_args = dict(shuffle=shuffle, batch_size=512, num_workers=num_workers, pin_memory=True) if cuda else dict(shuffle=True, batch_size=64)
     loader = torch.utils.data.DataLoader(data, **dataloader_args)
     return loader
+
+def get_model_summary():
+    model = Resnet18().to(device)
+    summary(model, input_size=(3, 32, 32))
---
+++ models/resnet18.py
@@ -0,0 +1,77 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Resnet18(nn.Module):
+    def __init__(self):
+        super(Resnet18, self).__init__()
+        self.base_channels = 64
+        self.block1 = nn.Sequential(
+            nn.Conv2d(3, self.base_channels, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(self.base_channels),
+            nn.Dropout(0.1),
+            nn.Conv2d(self.base_channels, self.base_channels, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(self.base_channels),
+            nn.Dropout(0.1),
+        )
+        self.mixer1 = nn.Conv2d(self.base_channels, self.base_channels*2, 1, stride=2)
+        self.prepblock1 = nn.Sequential(
+            nn.Conv2d(self.base_channels, self.base_channels*2, 3, stride=2),
+            nn.BatchNorm2d(self.base_channels*2),
+            nn.Dropout(0.1),
+        )
+        self.block2 = nn.Sequential(
+            nn.Conv2d(self.base_channels*2, self.base_channels*2, 3, padding=1),
+            nn.BatchNorm2d(self.base_channels*2),
+            nn.Dropout(0.1),
+        )
+        self.mixer2 = nn.Conv2d(self.base_channels*2, self.base_channels*4, 1, stride=2)
+        self.prepblock2 = nn.Sequential(
+            nn.Conv2d(self.base_channels*2, self.base_channels*4, 3, stride=2),
+            nn.BatchNorm2d(self.base_channels*4),
+            nn.Dropout(0.1),
+        )
+        self.block3 = nn.Sequential(
+            nn.Conv2d(self.base_channels*4, self.base_channels*4, 3, padding=1),
+            nn.BatchNorm2d(self.base_channels*4),
+            nn.Dropout(0.1),
+        )
+        self.mixer3 = nn.Conv2d(self.base_channels*4, self.base_channels*8, 1, stride=2)
+        self.prepblock3 = nn.Sequential(
+            nn.Conv2d(self.base_channels*4, self.base_channels*8, 3, stride=2),
+            nn.BatchNorm2d(self.base_channels*8),
+            nn.Dropout(0.1),
+        )
+        self.block4 = nn.Sequential(
+            nn.Conv2d(self.base_channels*8, self.base_channels*8, 3, padding=1),
+            nn.BatchNorm2d(self.base_channels*8),
+            nn.Dropout(0.1),
+        )
+
+    def forward(self, x):
+        res1 = self.block1(x)
+        x = x + res1
+        res2 = self.block1(x)
+        x = x + res2
+
+        res3 = self.block2(F.relu(self.prepblock1(x)))
+        x = self.mixer1(x)
+        x = x + res3
+        res4 = self.block2(self.block2(x))
+        x = F.relu(x + res4)
+
+        res5 = self.block3(F.relu(self.prepblock2(x)))
+        x = self.mixer2(x)
+        x = x + res5
+        res6 = self.block3(self.block3(x))
+        x = F.relu(x + res6)
+
+        res3 = self.block4(F.relu(self.prepblock3(x)))
+        x = self.mixer3(x)
+        x = x + res3
+        res4 = self.block2(self.block2(x))
+        x = F.relu(x + res4)
+
+
