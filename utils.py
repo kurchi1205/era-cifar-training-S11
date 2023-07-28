e51a7f836181e1eb49d6b09ceb67aac6c952322d
Prerana <prerana@hellosivi.com>
2023-07-28 21:52:21 +0530

    taking one image

--- utils.py
+++ utils.py
@@ -51,6 +51,7 @@ def visualize_cam(cam, rgb_img, input_tensor, img_id):
     targets = [ClassifierOutputTarget(9)]
     print(targets)
     grayscale_cam = cam(input_tensor=input_tensor, targets=targets)
+    grayscale_cam = grayscale_cam[0, :]
     visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
     print(visualization)
 
